b255c85dc178eae1ddecad8f8d998e688ef33d67
BhavyaGaharwar <bhavya0653@gmail.com>
2026-07-31 10:22:21 +0530

    Program 4

---
+++ prg4.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+    vector<int>numbers={10,34,24,97,54};
+    cout<<"elements of the collection are:"<<endl;;
+    for(auto num:numbers) {
+    cout<<num<<" ";
+}
+   cout<< endl;
+   return 0;
+}
