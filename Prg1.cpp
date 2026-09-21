61e0ad1e6c3003c52706ce8f31abb2bd17651edf
BhavyaGaharwar <bhavya0653@gmail.com>
2026-09-21 10:50:30 +0530

    commit

---
+++ Prg1.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+class Student
+{
+private:
+    int rollNo;
+    string name;
+    float marks;
+
+public:
+    void input()
+    {
+        cout << "Enter Roll No: ";
+        cin >> rollNo;
+
+        cout << "Enter Name: ";
+        cin >> name;
+
+        cout << "Enter Marks: ";
+        cin >> marks;
+    }
+
+    void display()
+    {
+        cout << "\nStudent Record" << endl;
+        cout << "Roll No: " << rollNo << endl;
+        cout << "Name: " << name << endl;
+        cout << "Marks: " << marks << endl;
+    }
+};
+
+int main()
+{
+    Student s;
+
+    s.input();
+    s.display();
+
+    return 0;
+}
